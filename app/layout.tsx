b9fce7277b886e6d2175d20ed0d49d03bd09446b
envaire <neuraaiworkspace@gmail.com>
2025-07-03 21:48:23 +0200

    Fix favicon for custom domain

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,9 +18,7 @@ export const metadata: Metadata = {
         sizes: 'any',
         type: 'image/x-icon',
       }
-    ],
-    shortcut: '/favicon.ico',
-    apple: '/favicon.ico',
+    ]
   }
 }
 
@@ -31,11 +29,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" href="/favicon.ico" />
-      </head>
       <body className={inter.className}>
         <LanguageProvider>
           <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
